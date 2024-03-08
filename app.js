bb0943439c68beb523712eac2f150e97044eb44c
Omari <lynomari@gmail.com>
2024-03-08 14:25:56 -0500

    Implement JavaScript functionality

--- app.js
+++ app.js
@@ -47,3 +47,7 @@ function init() {
     tasks = []; //Reset the tasks array
     render(); //Call the render function
 }
+//Event Listener for form submission
+taskForm.addEventListener('submit', handleSubmission);
+//Call the init function to set up the initial state of the app
+init();
